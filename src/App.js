01d2d6ece47eb0560632be6bdc10e79731df6bdc
Jonathan <jonathan.y.cai@outlook.com>
2024-11-10 10:21:52 -0800

    Added libraries and created Search Bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { CiSearch } from 'react-icons/ci';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Select Elect</h1>
+
+        <div>
+          <input></input>
+          <a><CiSearch /></a>
+        </div>
+
+
+
       </header>
     </div>
   );
